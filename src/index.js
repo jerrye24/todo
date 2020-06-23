89f060683fd3f4a607e5fd011a48b3bcf02ef8fa
jerrye24 <jerrye1986@gmail.com>
2020-06-23 15:46:26 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+    const items = ['Drink coffee', 'Build Awesome App']
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
         </ul>
     );
 };
@@ -15,12 +16,22 @@ const AppHeader = () => {
 };
 
 const SearchPanel = () => {
-    return <input placeholder="search" />;
+    const searchText = "Type here to search";
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return <input style={ searchStyle } placeholder={ searchText } className="foo"/>;
 };
 
 const App = () => {
+
+    const isLoggedIn = true;
+    const loginBox = <span>Log in, please</span>;
+    const welcomeBox = <span>Welcome Back</span>;
+
     return (
         <div>
+            { isLoggedIn ? welcomeBox : loginBox }
             <AppHeader />
             <SearchPanel />
             <TodoList />
