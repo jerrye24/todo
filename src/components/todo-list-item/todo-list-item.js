3d9f07bb2a1359ad39c3e0e237e52363954cda01
jerrye24 <jerrye1986@gmail.com>
2020-06-24 16:17:30 +0300

    Event handling

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`)
+  }
   
   render() {
 
@@ -17,7 +21,8 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={ this.onLabelClick }>
           {label}
         </span>
   
