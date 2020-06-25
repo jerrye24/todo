e75cae830e8ad17649c9b286fe5126886b6cc85f
jerrye24 <jerrye1986@gmail.com>
2020-06-25 16:53:17 +0300

    setState delete element

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -10,20 +10,24 @@ export default class TodoListItem extends Component {
   };
 
   onLabelClick = () => {
-    this.setState({
-      done: true
+    this.setState(({done}) => {
+      return {
+        done: !done
+      };
     });
   };
 
   onMarkImportant = () => {
-    this.setState({
-      important: true
+    this.setState(({important}) => {
+      return {
+        important: !important
+      };
     });
   };
   
   render() {
 
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done,  important} = this.state;
 
     let classNames = 'todo-list-item';
@@ -50,7 +54,8 @@ export default class TodoListItem extends Component {
         </button>
   
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
