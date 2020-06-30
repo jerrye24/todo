0db14e202e4e019c255419dabd92c91f8a428619
jerrye24 <jerrye1986@gmail.com>
2020-06-30 15:26:42 +0300

    Forms

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,14 +4,35 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        })
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
+
     render() {
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary"
-                        onClick={() => {this.props.onItemAdded('New item label')}}>
+            <form className="item-add-form input-group"
+                onSubmit={ this.onSubmit }>
+                    
+                <input type="text"
+                       className="form-control"
+                       onChange={ this.onLabelChange }
+                       placeholder="What needs to be done" />
+            
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     };
 
